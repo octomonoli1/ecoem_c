03e888d1a37ab103bf77b79664b141d831acbf8b
octomonoli1 <octavio.mo90@gmail.com>
2025-09-15 20:46:57 +0200

    Ejemplo sobre estructuras de control

---
+++ prog_estructurada/estructuras_de_control.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+
+    //SECUENCIA: Ejemplo que pide un nombre al usuario y lo imprime
+    char nombre[100];
+
+    printf("Introduce tu nombre: "); 
+    //scanf("%s", nombre); // Esto solo lee hasta el primer espacio
+
+    fgets(nombre, sizeof(nombre), stdin); // Lee una línea completa, incluyendo espacios
+
+    printf("Hola, %s\n", nombre);
+
+    //SELECCION: Ejemplo que pide un número y verifica si es par o impar    
+
+    int numero;
+    
+    printf("Introduce un numero entero: ");
+    scanf("%d", &numero);
+    if (numero % 2 == 0) {
+        printf("El numero %d es par.\n", numero);
+    } else {
+        printf("El numero %d es impar.\n", numero);
+    }
+
+    //ITERACION: Ejemplo que imprime los números del 1 al 10
+    printf("Numeros del 1 al 10:\n");
+    for (int i = 1; i <= 10; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    return 0;
+}
